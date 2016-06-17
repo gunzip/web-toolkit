68c6fddada49b6448b566d3a61395ef8735031e6
Danilo Spinelli <danilo.spinelli@agid.gov.it>
2016-06-17 19:18:54 +0200

    linklist

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,33 +1,24 @@
+import $ from 'jquery';
+
+const __exports = {};
+/*
+ * Accordion
+ * ================
+ */
 import Fraccordion from 'fr-accordion';
 import stylesheet from 'fr-accordion/accordion.css';
 
-const myAccordion = Fraccordion({
-	// String - Outer container selector, hook for JS init() method
-	selector: '.js-fr-accordion',
-
-	// String - Accordion header elements converted to focusable, togglable elements
-	headerSelector: '.js-fr-accordion__header',
-
+__exports.Accordion = Fraccordion({
 	// String - Use header id on element to tie each accordion panel to its header - see panelIdPrefix
 	headerIdPrefix: 'accordion-header',
-
-	// String - Accordion panel elements to expand/collapse
-	panelSelector: '.js-fr-accordion__panel',
-
 	// String - Use panel id on element to tie each accordion header to its panel - see headerIdPrefix
 	panelIdPrefix: 'accordion-panel',
-
 	// Boolean - If set to false, all accordion panels will be closed on init()
 	firstPanelsOpenByDefault: false,
-
 	// Boolean - If set to false, each accordion instance will only allow a single panel to be open at a time
 	multiselectable: false,
-
 	// String - Class name that will be added to the selector when the component has been initialised
-	readyClass: 'fr-accordion--is-ready',
-
-	// Integer - Duration (in milliseconds) of CSS transition when opening/closing accordion panels
-	transitionLength: 250
+	readyClass: 'fr-accordion--is-ready'
 });
 
 /*
@@ -36,13 +27,19 @@ const myAccordion = Fraccordion({
  */
 
 import Packery from 'packery';
-import $ from 'jquery';
+
+//	Without jquery
+//
+//	import 'imagesLoaded' from 'imagesloaded';
+//  imagesLoaded( document.querySelector('.js-packery'), function( instance ) {
+//    console.log('all images are loaded');
+//  });
 
 $(document).ready(function() {
 	var cnt = $("img").length;
   $("img").one("load", function() {
     if (--cnt === 0) {
-			var pckry = new Packery('.js-packery', {
+			__exports.myPackery = new Packery('.js-packery', {
 			  itemSelector: '.Card',
 			  gutter: 20
 			});
@@ -51,3 +48,5 @@ $(document).ready(function() {
     if (this.complete) $(this).load();
   });
 });
+
+export default __exports;
