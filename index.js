3f5b1e1514497fa4f4ea8724b6cffcca81cb1494
Danilo Spinelli <danilo.spinelli@agid.gov.it>
2016-06-30 12:01:42 +0200

    offcanvas

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import IwtAccordion from './src/modules/accordion';
 import IwtSkiplinks from './src/modules/skiplinks';
 import IwtPackery from './src/modules/packery';
 import IwtTreeview from './src/modules/treeview';
+import IwtOffcanvas from './src/modules/offcanvas';
 
 const __exports = {
 	IwtAccordion,
 	IwtPackery,
 	IwtTreeview,
 	IwtSkiplinks,
+	IwtOffcanvas,
 };
 
 export default __exports;
