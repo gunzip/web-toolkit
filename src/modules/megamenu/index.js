e0174f0f00a4b876af4f8a3a2a6ce1f684deb909
Danilo Spinelli <danilo.spinelli@agid.gov.it>
2016-07-08 16:13:19 +0200

    megamenu

---
+++ src/modules/megamenu/index.js
@@ -0,0 +1,5 @@
+import scriptjs from 'scriptjs';
+
+scriptjs('https://cdn.rawgit.com/adobe-accessibility/Accessible-Mega-Menu/master/js/jquery-accessibleMegaMenu.js', function() {
+  alert('yo');
+});
