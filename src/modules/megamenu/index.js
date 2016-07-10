dbfa86ec88ccf78b41b216cd8f4ee8215d06ea04
Danilo Spinelli <danilo.spinelli@agid.gov.it>
2016-07-10 11:47:18 +0200

    ok header clone

--- src/modules/megamenu/index.js
+++ src/modules/megamenu/index.js
@@ -1,40 +1,83 @@
-/* global $ */
-
+import $ from 'jquery';
 import scriptjs from 'scriptjs';
+
+// ASAP hide megamenu panels
+$('.js-megamenu').addClass('is-ready');
+
+const opts = {
+  /* prefix for generated unique id attributes, which are required
+     to indicate aria-owns, aria-controls and aria-labelledby */
+  uuidPrefix: 'megamenu',
+
+  /* css class used to define the megamenu styling */
+  menuClass: 'Megamenu',
+
+  menuListClass: 'Megamenu-list',
+
+  /* css class for a top-level navigation item in the megamenu */
+  topNavItemClass: 'Megamenu-item',
+
+  /* css class for a megamenu panel */
+  panelClass: 'Megamenu-subnav',
+
+  /* css class for a group of items within a megamenu panel */
+  panelGroupClass: 'Megamenu-subnavGroup',
+
+  /* css class for the hover state */
+  hoverClass: 'is-hover',
+
+  /* css class for the focus state */
+  focusClass: 'is-focus',
+
+  /* css class for the open state */
+  openClass: 'is-open'
+};
+
+/*
+ *	Take a Treeview and make it work with Megamenu
+ */
+const listToMegaMenu = ($ul, _opts) => {
+  const attrs = 'class aria-expanded aria-hidden role tabindex';
+  return $ul
+    .clone()
+    .removeAttr(attrs)
+    .addClass(_opts.menuListClass)
+    .find('*')
+      .removeAttr(attrs)
+      .end()
+    .find('> li')
+    .each(function(i, li) {
+      $(li)
+        .addClass(_opts.topNavItemClass)
+        .find('a')
+          // make item tabbable, this is required !
+          .attr('href', '#')
+        .end()
+        .find('> ul > li')
+          .unwrap()
+          .wrap(`<ul class="${_opts.panelGroupClass}" />`)
+        .end()
+        .find('> ul')
+        .wrapAll(`<div class="${_opts.panelClass}" />`)
+    })
+    .end();
+}
 
 /* FIXME: wait till https://github.com/adobe-accessibility/Accessible-Mega-Menu/pull/38 and then packege with npm */
 
-$('.js-megamenu')
-  .addClass('is-ready');
-
 scriptjs('https://cdn.rawgit.com/adobe-accessibility/Accessible-Mega-Menu/master/js/jquery-accessibleMegaMenu.js', function() {
 
-  $('.js-megamenu')
-    .accessibleMegaMenu({
-    /* prefix for generated unique id attributes, which are required
-       to indicate aria-owns, aria-controls and aria-labelledby */
-    uuidPrefix: 'megamenu',
-
-    /* css class used to define the megamenu styling */
-    menuClass: 'Megamenu',
-
-    /* css class for a top-level navigation item in the megamenu */
-    topNavItemClass: 'Megamenu-item',
-
-    /* css class for a megamenu panel */
-    panelClass: 'Megamenu-subnav',
-
-    /* css class for a group of items within a megamenu panel */
-    panelGroupClass: 'Megamenu-subnavGroup',
-
-    /* css class for the hover state */
-    hoverClass: 'is-hover',
-
-    /* css class for the focus state */
-    focusClass: 'is-focus',
-
-    /* css class for the open state */
-    openClass: 'is-open'
+  $('.js-megamenu').each((i, el) => {
+    const $el = $(el);
+    const rel = $(el).data('rel');
+    if ($el.find('ul').length === 0 && rel && $(rel).length > 0) {
+      let $menu = listToMegaMenu($(rel), opts);
+      $el.append($menu);
+      console.log($el.html());
+    }
+    $el.accessibleMegaMenu(opts);
   });
 
 });
+
+module.exports = { listToMegaMenu };
