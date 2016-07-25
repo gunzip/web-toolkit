c400cdd9761b3f75495a754cf8071491cdc5be31
Danilo Spinelli <danilo.spinelli@agid.gov.it>
2016-07-25 15:38:34 +0200

    form fix

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,12 +1,14 @@
 import $ from 'jquery'
 
-$(':radio, :checkbox').on('change', function() {
-  if ($(this).is(':radio')) {
-    $(this).closest('.Form-field').find('.Form-label.is-checked').removeClass('is-checked')
-  }
-  if ($(this).is(':checked')) {
-    $(this).closest('.Form-label').addClass('is-checked')
-  } else {
-    $(this).closest('.Form-label').removeClass('is-checked')
-  }
+$(document).ready(function() {
+  $(':radio, :checkbox').on('change', function() {
+    if ($(this).is(':radio')) {
+      $(this).closest('.Form-field').find('.Form-label.is-checked').removeClass('is-checked')
+    }
+    if ($(this).is(':checked')) {
+      $(this).closest('.Form-label').addClass('is-checked')
+    } else {
+      $(this).closest('.Form-label').removeClass('is-checked')
+    }
+  })
 })
