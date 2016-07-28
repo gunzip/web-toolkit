a548ff44f96aca97ea89f63e42af79d284cbd681
Danilo Spinelli <danilo.spinelli@agid.gov.it>
2016-07-28 17:10:15 +0200

    fix share

--- src/modules/share/index.js
+++ src/modules/share/index.js
@@ -2,15 +2,14 @@ import $ from 'jquery'
 
 const hideClass = 'u-jsHiddenVisually'
 
-$(document).ready(() => {
-  $('.Share > ul')
-    .find('li')
-    .css({
-      maxWidth: 0
-    })
-})
+$('.js-Share').click((e) => {
+  const $el = $(e.currentTarget)
+  const $share = $el.closest('.Share')
+  const $buttons = $share.find('ul')
 
-const _toggleVisibility = ($share) => {
+  $buttons
+    .toggleClass(hideClass)
+
   $share
     .find('.Share-revealText')
     .toggleClass(hideClass)
@@ -18,39 +17,4 @@ const _toggleVisibility = ($share) => {
   $share
     .find('.Share-revealIcon')
     .toggleClass('is-open')
-}
-
-
-$('.js-Share').click((e) => {
-  const $el = $(e.currentTarget)
-  const $share = $el.closest('.Share')
-  const $buttons = $share.find('ul')
-
-  if ($buttons.hasClass(hideClass)) {
-
-    _toggleVisibility($share)
-
-    $buttons
-      .removeClass(hideClass)
-      .find('li')
-      .animate({
-        maxWidth: '200px'
-      },
-      250)
-
-  } else {
-
-    $buttons
-      .find('li')
-      .animate({
-          maxWidth: 0
-        },
-        250,
-        () => {
-          $buttons.addClass(hideClass)
-          _toggleVisibility($share)
-        })
-
-  }
-
 })
