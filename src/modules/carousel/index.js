ed15d91674fa5bb014252bf6865ee57e5dac630c
Danilo Spinelli <danilo.spinelli@agid.gov.it>
2016-07-29 16:01:33 +0200

    carousel

--- src/modules/carousel/index.js
+++ src/modules/carousel/index.js
@@ -8,16 +8,21 @@ import owlCarousel from 'owl.carousel'
 
 /* eslint-enable no-unused-vars */
 
-$(function() {
-
-  $('.js-Carousel').owlCarousel({
-    nav: true,
-    navText: ['precedente', 'successivo'],
+const opts = {
+  owlPrev: '.owl-prev',
+  owlNext: '.owl-next',
+  owlItem: '.owl-item',
+  jsSelector: '.js-Carousel',
+  owlOpts: {
+    nav: false,
+    // navText: ['precedente', 'successivo'],
     items: 3,
     dots: false,
     loop: false,
     margin: 16,
     mouseDrag: true,
+    URLhashListener: true,
+    startPosition: 'URLHash',
     responsive: {
       0: {
         items: 1
@@ -29,7 +34,29 @@ $(function() {
         items: 3
       }
     }
+  }
+}
+
+const init = function() {
+  const $owl = $(opts.jsSelector).owlCarousel(opts.owlOpts)
+  $(opts.owlPrev).click(() => $owl.trigger('prev.owl.carousel'))
+  $(opts.owlNext).click(() => $owl.trigger('next.owl.carousel'))
+
+  // $owl.find(opts.owlItem).attr('tabindex', '0')
+
+  $owl.on('initialized.owl.carousel changed.owl.carousel refreshed.owl.carousel', (event) => {
+    if (!event.namespace) return
+    var carousel = event.relatedTarget,
+      element = event.target,
+      current = carousel.current()
+    $(opts.owlPrev, element).toggleClass('disabled', current === carousel.maximum())
+    $(opts.owlNext, element).toggleClass('disabled', current === carousel.minimum())
   })
+}
 
+$(document).ready(init)
 
-})
+export default {
+  opts,
+  init
+}
