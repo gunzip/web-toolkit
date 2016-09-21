0d97d03e2d1f50d3566c8930ed3db64ea0bfc140
Danilo Spinelli <danilo.spinelli@agid.gov.it>
2016-09-21 18:11:39 +0200

    fix breadcrumb

--- src/modules/header/index.js
+++ src/modules/header/index.js
@@ -64,6 +64,10 @@ if (myElement) {
 const headroomFixed = '.Headroom--fixed'
 
 if ($('.' + opts.classes.initial).is(headroomFixed)) {
+  const INTERVAL = 250
+
+  let windowWidth = $(window).width()
+
   // Needs to be here due to CSS transition (see on Safari)
   let headerHeight = $(headroomFixed).height()
 
@@ -85,20 +89,23 @@ if ($('.' + opts.classes.initial).is(headroomFixed)) {
   })
 
   // Make padding respond to window resize
-  // Android browser triggers a resize event on scroll to top
-  $(window).resize(debounce(250, function() {
+  $(window).resize(debounce(INTERVAL, function() {
+    const newWindowWidth = $(window).width()
     const height = $(headroomFixed).height()
-    if (headerHeight < height) {
+    // Android browser triggers a resize event on scroll to top
+    // so we check for changes in window width
+    if (newWindowWidth !== windowWidth) {
+      windowWidth = newWindowWidth
       headerHeight = height
-      setTimeout(_adjustPadding, 250)
+      setTimeout(_adjustPadding, INTERVAL)
     }
   }))
 
-  // This happens *only* after a resize
-  // when scrolling to top
-  $(headroomFixed).on('transitionend', debounce(250, function() {
+  $(headroomFixed).on('transitionend', debounce(INTERVAL, function() {
     const height = $(this).height()
     if (headerHeight < height) {
+      // This happens *only* after a resize
+      // _and_ when scrolling to top
       headerHeight = height
       _adjustPadding()
     }
